63704e1c9b3f059dd20b105928e2ca76abe10a46
unknown <boylekacey984@gmail.com>
2024-08-08 02:54:47 +1000

    Create Splash Screen

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
+        SplashScreen splash = new SplashScreen(5000);
+        splash.displayAndExit();
+        /*
         System.out.print("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
@@ -11,5 +14,6 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+         */
     }
 }
---
+++ src/SplashScreen.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class SplashScreen extends JWindow {
+    private int m_Duration;
+
+    public SplashScreen(int duration){
+        m_Duration = duration;
+        setup();
+    }
+    private void setup(){
+        JPanel panel = (JPanel) getContentPane();
+        // Centre splash screen on screen
+        int width = 300;
+        int height = 500;
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension screenSize = toolkit.getScreenSize();
+        int x = screenSize.width / 2 - width / 2;
+        int y = screenSize.height / 2 - height / 2;
+        setBounds(x, y, width, height);
+    }
+    private void showSplash(){
+        setVisible(true);
+        try{
+            Thread.sleep(m_Duration);
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+    public void displayAndExit(){
+        showSplash();
+        System.exit(0);
+    }
+}
