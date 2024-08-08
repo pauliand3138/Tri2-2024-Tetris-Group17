c78c5686a9c2e6dd7174904fce8b10616ef77649
unknown <boylekacey984@gmail.com>
2024-08-08 18:32:14 +1000

    Add Splash Image To Project

--- src/SplashScreen.java
+++ src/SplashScreen.java
@@ -11,7 +11,7 @@ public class SplashScreen extends JWindow {
     }
     private void displaySplash(){
         JPanel panel  = (JPanel) getContentPane();
-        // Centre panel on screen
+        // Centre panel on splash screen
         int width = 300;
         int height = 500;
         Toolkit toolkit = Toolkit.getDefaultToolkit();
@@ -19,5 +19,11 @@ public class SplashScreen extends JWindow {
         int x = screenSize.width / 2 - width / 2;
         int y = screenSize.height / 2 - height / 2;
         setBounds(x, y, width, height);
+        // Create and add components to splash screen
+        JLabel splashImage = new JLabel(new ImageIcon());
+        JLabel creators = new JLabel("Creators: Janet Chimwayange, Paul Ian Lim, Karan Singde, Darcy McIntosh, Kacey Boyle.");
+        JLabel description = new JLabel();
+
+        setVisible(true);
     }
 }
