4594b5a48f799d6dc89ae42406ba14405d5ae283
unknown <boylekacey984@gmail.com>
2024-08-08 18:20:48 +1000

    Centre Splash Screen

--- src/SplashScreen.java
+++ src/SplashScreen.java
@@ -5,4 +5,19 @@ public class SplashScreen extends JWindow {
     private final int m_Duration = 3000;
     public SplashScreen(){
     }
+    public void displaySplashAndExit(){
+        displaySplash();
+        System.exit(0);
+    }
+    private void displaySplash(){
+        JPanel panel  = (JPanel) getContentPane();
+        // Centre panel on screen
+        int width = 300;
+        int height = 500;
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension screenSize = toolkit.getScreenSize();
+        int x = screenSize.width / 2 - width / 2;
+        int y = screenSize.height / 2 - height / 2;
+        setBounds(x, y, width, height);
+    }
 }
