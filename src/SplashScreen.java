fe8a47d7f8029b89a2c469203017793399b33f18
unknown <boylekacey984@gmail.com>
2024-08-08 19:34:49 +1000

    Create Splash Screen Components

--- src/SplashScreen.java
+++ src/SplashScreen.java
@@ -4,13 +4,15 @@ import java.awt.*;
 public class SplashScreen extends JWindow {
     private final int m_Duration = 3000;
     public SplashScreen(){
+        setup();
     }
     public void displaySplashAndExit(){
         displaySplash();
         System.exit(0);
     }
-    private void displaySplash(){
+    private void setup(){
         JPanel panel  = (JPanel) getContentPane();
+        panel.setLayout(new BorderLayout());
         // Centre panel on splash screen
         int width = 300;
         int height = 500;
@@ -20,10 +22,20 @@ public class SplashScreen extends JWindow {
         int y = screenSize.height / 2 - height / 2;
         setBounds(x, y, width, height);
         // Create and add components to splash screen
-        JLabel splashImage = new JLabel(new ImageIcon());
+        JLabel splashImage = new JLabel(new ImageIcon("src\\splash.png"));
+        panel.add(splashImage, BorderLayout.CENTER);
         JLabel creators = new JLabel("Creators: Janet Chimwayange, Paul Ian Lim, Karan Singde, Darcy McIntosh, Kacey Boyle.");
-        JLabel description = new JLabel();
-
+        panel.add(creators, BorderLayout.NORTH);
+        JLabel description = new JLabel("Description: One of seven tetrimino blocks are chosen. Once ");
+        panel.add(description, BorderLayout.SOUTH);
+    }
+    private void displaySplash(){
         setVisible(true);
+        try{
+            Thread.sleep(m_Duration);
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+        setVisible(false);
     }
 }
