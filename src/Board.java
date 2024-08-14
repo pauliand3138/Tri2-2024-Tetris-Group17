a7fadf56e4c6a9008aa0dcda70f4621a9893c296
Kacey <boylekacye984@gmail.com>
2024-08-14 14:41:57 +1000

    add grid cell rectangles to board

--- src/Board.java
+++ src/Board.java
@@ -2,15 +2,32 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Board extends JPanel {
-    public Board(){
-        initialize();
+    private final int ROW_COUNT = 20;
+    private final int COL_COUNT = 10;
+    private int gridCellSize;
+    public Board(int width, int height){
+        initialize(width, height);
     }
-    private void initialize(){
+    public int getBoardHeight(){
+        return getHeight();
+    }
+    private void initialize(int width, int height){
+        int boardWidth = (int)(width * 0.35F);
+        gridCellSize = boardWidth / COL_COUNT;
+        int boardHeight = (int)(gridCellSize * ROW_COUNT);
+        int x = width / 2 - boardWidth / 2;
+        int y = (int)(height * 0.10F);
+        setBounds(x, y, boardWidth, boardHeight);
         setBorder(BorderFactory.createLoweredBevelBorder());
     }
     @Override
     protected void paintComponent(Graphics g){
         super.paintComponent(g);
-        g.fillRect(0,0, 10, 10);
+        // Draw's cell on's grid
+        for(int row = 0 ; row < ROW_COUNT ; row++){
+            for(int col = 0; col < COL_COUNT ; col++){
+                g.drawRect(col * gridCellSize, row * gridCellSize, gridCellSize, gridCellSize);
+            }
+        }
     }
 }
--- src/Play.java
+++ src/Play.java
@@ -15,25 +15,22 @@ public class Play extends JPanel {
         int width = 500;
         int height = 500;
         int halfWidth = width / 2;
-        int x, y;
 
         JLabel playLabel = new JLabel("Play");
         playLabel.setFont(new Font("Arial", Font.BOLD, 20));
         playLabel.setBounds(halfWidth - 25, 10, 50, 25);
         add(playLabel);
 
-        Board board = new Board();
-        int boardWidth = (int) (width * 0.35F);
-        int boardHeight = (int) (height * 0.70F);
-        x = halfWidth - boardWidth / 2;
-        y = (int) (0.10F * height);
+        Board board = new Board(width, height);
         add(board);
-        board.setBounds(x, y, boardWidth, boardHeight);
         board.setVisible(true);
 
+        /*
         JLabel authorsLabel = new JLabel("Authors: Paul Ian Lim, Karan Singde, Janet Chimwayange, Darcy McIntosh and Kacey Boyle.");
-        authorsLabel.setBounds(halfWidth - authorsLabel.getWidth() / 2, 0, 10, 10);
         authorsLabel.setFont(new Font("Arial", Font.BOLD, 10));
+        Dimension authorsLabelDimension = authorsLabel.getPreferredSize();
+        authorsLabel.setBounds(width - authorsLabelDimension.width, y + board.getBoardHeight() + 15, authorsLabelDimension.width, authorsLabelDimension.height);
         add(authorsLabel);
+         */
     }
 }
