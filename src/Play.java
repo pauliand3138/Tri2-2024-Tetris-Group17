bd4af308b724d7e84227bd212a2406984a9870f6
Kacey <boylekacye984@gmail.com>
2024-08-15 16:41:59 +1000

    add back button to play screen class

--- src/Play.java
+++ src/Play.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 
 
@@ -14,10 +16,9 @@ public class Play extends JPanel {
     private void addElements(){
         int width = 500;
         int height = 500;
-        int halfWidth = width / 2;
 
-        float titlePanelPct = 0.30F;
-        float boardPanelPct = 0.60F;
+        float titlePanelPct = 0.20F;
+        float boardPanelPct = 0.50F;
         float creatorsPanelPct = 1 - boardPanelPct - titlePanelPct;
 
         int titlePanelHeight = (int)(height * titlePanelPct);
@@ -38,10 +39,17 @@ public class Play extends JPanel {
 
         int creatorsPanelHeight = (int)(height * creatorsPanelPct);
         JPanel creatorsPanel = new JPanel();
+        creatorsPanel.setLayout(new BorderLayout());
         creatorsPanel.setSize(width, creatorsPanelHeight);
+        JButton backButton = new JButton("Back");
+        backButton.setPreferredSize(new Dimension(width, 20));
+        backButton.setBackground(Color.WHITE);
+        creatorsPanel.add(backButton, BorderLayout.NORTH);
+
         JLabel creatorsLabel = new JLabel("Creators: Paul Ian Lim, Karan Singde, Darcy McIntosh, Janet Chimwayange and Kacey Boyle.", SwingConstants.CENTER);
-        creatorsLabel.setFont(new Font("Calibri", Font.BOLD, 12));
-        creatorsPanel.add(creatorsLabel);
+        creatorsLabel.setFont(new Font("Calibri", Font.PLAIN, 12));
+        creatorsPanel.add(creatorsLabel, BorderLayout.SOUTH);
+
         add(creatorsPanel, BorderLayout.SOUTH);
     }
 }
