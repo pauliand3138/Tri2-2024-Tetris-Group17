cab27f3448c2bf0f0c994cea79051242c9de7a18
Paul <paul.iand3138@hotmail.com>
2024-09-24 17:20:55 +0800

    Add OpMove and PureGame class for External Player Implementation

---
+++ src/OpMove.java
@@ -0,0 +1,17 @@
+public class OpMove{
+    private int opX;
+    private int opRotate;
+
+    public OpMove(int opX, int opRotate){
+        this.opX = opX;
+        this.opRotate = opRotate;
+    }
+
+    public int getOpX() {
+        return this.opX;
+    }
+
+    public int getOpRotate() {
+        return this.opRotate;
+    }
+}
---
+++ src/PureGame.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+public class PureGame {
+    private int width;
+    private int height;
+    private int[][] cells;
+    private int[][] currentShape;
+    private int[][] nextShape;
+
+    public PureGame(int height, int width) {
+        this.height = height;
+        this.width = width;
+        this.cells = new int[height][width];
+    }
+
+    public void setCells(int[][] cells) {
+        for (int y = 0; y < this.height; y++) {
+            for (int x = 0; x < this.width; x++)
+                this.cells[y][x] = cells[y][x];
+        }
+    }
+
+    public void setCurrentShape(int[][] currentShape) {
+        this.currentShape = currentShape;
+    }
+
+    public void setNextShape(int[][] nextShape) {
+        this.nextShape = nextShape;
+    }
+
+    @Override
+    public String toString() {
+        return "PureGame{" +
+                "width=" + width +
+                ", height=" + height +
+                ", cells=" + Arrays.deepToString(cells) +
+                ", currentShape=" + Arrays.deepToString(currentShape) +
+                ", nextShape=" + Arrays.deepToString(nextShape) +
+                '}';
+    }
+}
+
