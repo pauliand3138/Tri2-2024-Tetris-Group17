8b98192caa9d15d6d4fc236ec5898ce2f304713f
Jemimasmum <janchim_au@yahoo.com>
2024-09-28 15:45:15 +1000

    updating to implement Facade design pattern

---
+++ src/view/panel/gameFacade.java
@@ -0,0 +1,31 @@
+package facade;
+
+import model.GameInfo;
+import view.panel.GameInfoPanel;
+
+public class GameFacade {
+    private GameInfoPanel gameInfoPanel;
+    private GameInfo gameInfo;
+
+    public GameFacade(int playerNum, int playerType) {
+        this.gameInfo = new GameInfo(playerNum, playerType);
+        this.gameInfoPanel = new GameInfoPanel(gameInfo);
+    }
+
+    public void clearLines() {
+        gameInfoPanel.clearLines();
+    }
+
+    public void updateGameInfo() {
+        gameInfoPanel.updateGameInfo();
+    }
+
+    public GameInfoPanel getGameInfoPanel() {
+        return gameInfoPanel;
+    }
+
+    public GameInfo getGameInfo() {
+        return gameInfo;
+    }
+}
+
